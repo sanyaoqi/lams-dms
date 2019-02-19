91e1dfc9a2c7f32c2acecf600013a784a5ad24ea
韩冲 <chong.han@f2pool.com>
2019-02-19 18:23:22 +0800

    维护记录详情，未完待续

--- src/views/DeviceMaintainDetail.vue
+++ src/views/DeviceMaintainDetail.vue
@@ -1,20 +1,38 @@
 <template>
-  <!-- TODO 详情页样式 -->
   <div>
     <x-header>维护记录详情</x-header>
+    <swiper :list="demo01_list" v-model="demo01_index" @on-index-change="demo01_onIndexChange"></swiper>
   </div>
 
 </template>
 
 <script>
-  import { XHeader } from 'vux'
+  import { XHeader, Swiper, SwiperItem } from 'vux'
 
   export default {
     name: 'DeviceMaintainDetail',
     components: {
-      XHeader
+      XHeader,
+      Swiper,
+      SwiperItem
+    },
+    methods: {
+      demo01_onIndexChange (index) {
+        this.demo01_index = index
+      }
+    },
+    data () {
+      return {
+        demo01_index: 0,
+        demo01_list: baseList
+      }
     }
   }
+
+  const baseList = [{
+    url: 'javascript:',
+    img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg'
+  }]
 </script>
 
 <style scoped>
