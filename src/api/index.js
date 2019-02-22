b86871ae1084db394ca4fe5483216a58cf6480f0
陈桢 <chenzhen@MacBook.local>
2019-02-22 09:44:29 +0800

    add api config

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,12 @@ const project = ''
 const prefix = host + project + ''
 
 let apis = {
-  // d
-  devices: prefix + 'devices',
-  info: prefix + 'info'
+  devices: prefix + 'devices',			//设备列表 和 设备搜索（注意：搜索时必须带上keyword参数）
+  info: prefix + 'devices/',			//设备详情
+  filters: prefix + 'devices/filters', 	//筛选
+  jsticket: prefix + 'site/jsticket', 	//js_ticket
+  login: prefix + 'user/weworklogin',	//登录
+  userinfo: prefix + 'user/view',	//登录(参数传token)
 }
 
 export default apis
