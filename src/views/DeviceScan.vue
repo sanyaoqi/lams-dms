c2d7bb1244e553137505f783f0e78b9bf820dc50
陈桢 <chenzhen@MacBook.local>
2019-02-27 00:39:42 +0800

    扫码测试页

---
+++ src/views/DeviceScan.vue
@@ -0,0 +1,33 @@
+<template>
+      <button @click="clickTest">测试扫一扫</button>
+</template>
+
+<script>
+    export default {
+      name: 'device-scan',
+      methods: {
+        clickTest () {
+          this.$wechat.scanQRCode({
+            desc: 'scanQRCode desc',
+            needResult: 0,
+            scanType: ['qrCode', 'barCode'],
+            success: function (res) {
+              console.log('success')
+            },
+            error: function (res) {
+              if (res.errMsg.indexOf('function_not_exist') > 0) {
+                alert('版本过低请升级')
+              }
+            }
+          })
+        }
+      }
+    }
+</script>
+
+<style type="text/css" lang="less">
+
+</style>
+
+<style scoped>
+</style>
