1e6bd99d98603302ed578d3f456d0a29066cd477
韩冲 <chong.han@f2pool.com>
2019-02-27 13:19:22 +0800

    wechat config 调整到 app.vue

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,38 @@
 </template>
 
 <script>
+import api from '@/api'
+const SIGNURL = 'http://device.olfu.xyz/device-scan'
+
 export default {
-  name: 'app'
+  name: 'app',
+  mounted () {
+    this.axios
+      .get(api.jssign + '?url=' + SIGNURL)
+      .then(response => {
+        console.log(response)
+        this.json.time_stamp = response.data.data.timestamp
+        this.json.noncestr = response.data.data.nonce_str
+        this.json.signature = response.data.data.signature
+        this.$wechat.config({
+          beta: true,
+          debug: true,
+          appId: 'wwa417d520fd30a759',
+          timestamp: this.json.time_stamp,
+          nonceStr: this.json.noncestr,
+          signature: this.json.signature,
+          jsApiList: ['scanQRCode']
+        })
+        this.$wechat.ready(() => {
+          this.$store.commit('setConfigFlag')
+        })
+      })
+  },
+  data () {
+    return {
+      json: {}
+    }
+  }
 }
 </script>
 
