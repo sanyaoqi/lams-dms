d013318b8057452c44c1d0965add54edb8a111a8
陈桢 <wowzhaunyonh@gmail.com>
2019-03-02 17:30:33 +0800

    test token

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,8 @@
   export default {
     name: 'Home',
     created () {
+      // 测试方法
+      // this.getTestToken()
       if (window.localStorage.getItem('token')) {
         this.$router.push({path: '/devices'})
       } else {
@@ -22,6 +24,31 @@
             console.log(error)
           })
           .finally()
+      }
+    },
+    methods: {
+      getTestToken: function () {
+        this.axios
+          .post(api.weworkToken, {wework_id: 'ChenZhen'})
+          .then(response => {
+            window.localStorage.setItem('token', response.data.token)
+            if (window.localStorage.getItem('token')) {
+              this.$router.push({path: '/devices'})
+            } else {
+              this.axios
+                .get(api.jssign + '?url=' + window.location.href)
+                .then(response => {
+                  this.json.time_stamp = response.data.data.timestamp
+                  this.json.noncestr = response.data.data.nonce_str
+                  this.json.signature = response.data.data.signature
+                  window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + CORPID + '&redirect_uri=' + REDIRECT_URI + '&response_type=code&scope=snsapi_base&agentid=' + AGENTID + '&state=STATE#wechat_redirect'
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+                .finally()
+            }
+          })
       }
     },
     data () {
