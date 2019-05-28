c89f887c042ddec91f46c17745c120a9408d9002
Administrator <admin@example.com>
2019-05-28 13:03:27 +0800

    reports not back button

--- src/App.vue
+++ src/App.vue
@@ -18,68 +18,6 @@ export default {
 <style lang="less">
 @import '~vux/src/styles/reset.less';
 @import '~vux/src/styles/weui/weui.less';
+@import './assets/index.less';
 
-body {
-  background-color: #fbf9fe;
-}
-
-#add-repair {
-  right: 50%;
-  bottom: 20px;
-  width: 50px;
-  height: 50px;
-  background-color: #04be02;
-  z-index: 1;
-  display: block;
-  position: fixed;
-  border-radius: 50%;
-  color: #fff;
-  border: none;
-  margin-right: -25px;
-}
-#add-comment {
-  right: 50%;
-  bottom: 20px;
-  width: 50px;
-  height: 50px;
-  background-color: #04be02;
-  z-index: 1;
-  display: block;
-  position: fixed;
-  border-radius: 50%;
-  color: #fff;
-  border: none;
-  margin-right: -25px;
-}
-.textRed {
-  background-color: tomato;
-}
-.textOrange {
-  background-color: gold;
-}
-.textBlue {
-  background-color: deepskyblue;
-}
-.textGreen {
-  background-color: mediumspringgreen;
-}
-.textGray {
-  background-color: #E5E5E5;
-}
-.no-more {
-  text-align: center;
-}
-.go-top {
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  border-radius: 50%;
-  background: #3cc51f;
-  border: none;
-  width: 30px;
-  height: 30px;
-  color: #fff;
-  font-size: 20px;
-  padding-bottom: 3px;
-}
 </style>
